730fd56b9a34b8669047cfc402ec7be2b7967f97
Filip Mares <filipmares@gmail.com>
2011-09-26 17:16:58 -0700

    Initial commit

---
+++ open_pandora.js
@@ -0,0 +1,18 @@
+
+//Kill ads :)
+try{
+    console.log('Killing ad_container');
+    document.getElementById('ad_container').style.display = 'none';    
+}catch(err){
+
+}
+
+try{
+    console.log('Killing adContainer');    
+    document.getElementById('adContainer').style.display = 'none';
+}catch(err){
+
+}    
+
+
+
